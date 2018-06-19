28b69b4d01cdd2cd81fcacd6395c1056cdc8b770
Ryen <ryen.beatty@researchgate.net>
2018-06-19 15:02:28 +0200

    first commit

--- src/my-command.js
+++ src/my-command.js
@@ -1,34 +1,33 @@
-import BrowserWindow from 'sketch-module-web-view'
-const UI = require('sketch/ui')
+import BrowserWindow from "sketch-module-web-view";
+const UI = require("sketch/ui");
 
 export default function(context) {
-
   const options = {
-    identifier: 'unique.id',
+    identifier: "unique.id",
     width: 240,
     height: 180,
     show: false
-  }
+  };
 
-  var browserWindow = new BrowserWindow(options)
+  var browserWindow = new BrowserWindow(options);
 
   // only show the window when the page has loaded
-  browserWindow.once('ready-to-show', () => {
-    browserWindow.show()
-  })
+  browserWindow.once("ready-to-show", () => {
+    browserWindow.show();
+  });
 
-  const webContents = browserWindow.webContents
+  const webContents = browserWindow.webContents;
 
   // print a message when the page loads
-  webContents.on('did-finish-load', () => {
-    UI.message('UI loaded!')
-  })
+  webContents.on("did-finish-load", () => {
+    UI.message("UI loaded!");
+  });
 
   // add a handler for a call from web content's javascript
-  webContents.on('nativeLog', (s) => {
-    UI.message(s)
-    webContents.executeJavaScript(`setRandomNumber(${Math.random()})`)
-  })
+  webContents.on("nativeLog", s => {
+    UI.message(s);
+    webContents.executeJavaScript(`setRandomNumber(${Math.random()})`);
+  });
 
-  browserWindow.loadURL(require('../resources/webview.html'))
+  browserWindow.loadURL("https://microsoft.github.io/monaco-editor/");
 }
